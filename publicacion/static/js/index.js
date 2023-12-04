20b22d18b83fd9868326e85134471fd6d371b400
Nicolás Hernández <sistemas@actores.org.co>
2023-12-04 11:31:10 -0500

    changed

--- publicacion/static/js/index.js
+++ publicacion/static/js/index.js
@@ -1,26 +1,30 @@
 dataTable = $("#tablaSeries").DataTable({
     language: {
-        url: "static/js/es-Es.json"
+        // url: "static/js/es-Es.json"
+        url: "https://actores.org.co/dev/prescripcion/static/js/es-ES.json"
     }
 });
 
 dataTable = $("#tablaExplotaciones").DataTable({
     language: {
-        url: "../static/js/es-Es.json"
+        // url: "../static/js/es-Es.json"
+        url: "https://actores.org.co/dev/prescripcion/static/js/es-ES.json"
     }
 });
 
 
 dataTable = $("#tablaNoIdentificados").DataTable({
     language: {
-        url: "/../static/js/es-Es.json"
+        // url: "/../static/js/es-Es.json"
+        url: "https://actores.org.co/dev/prescripcion/static/js/es-ES.json"
     }
 });
 
 
 dataTable = $("#tablaNoSocios").DataTable({
     language: {
-        url: "/../static/js/es-Es.json"
+        // url: "/../static/js/es-Es.json"
+        url: "https://actores.org.co/dev/prescripcion/static/js/es-ES.json"
     }
 });
 
