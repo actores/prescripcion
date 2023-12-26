b2b72e639a717a2d255378eff94b835ba1a10af6
Nicolás Hernández <sistemas@actores.org.co>
2023-12-26 09:30:48 -0500

    Updated

--- publicacion/static/js/index.js
+++ publicacion/static/js/index.js
@@ -30,7 +30,7 @@ const dataTableOptions = {
     columnDefs: [
         { orderable: false, targets: [2] },
     ],
-    pageLength: 50,
+    pageLength: 10,
     destroy: true
 }
 
@@ -41,6 +41,7 @@ const initDatatable = async () => {
 
     await listarSeries();
     dataTable = $('#tablaSeries').DataTable(dataTableOptions);
+   
     dataTableIsInitialized = true;
 
     setTimeout(() => {
@@ -49,41 +50,36 @@ const initDatatable = async () => {
         loader.style.zIndex = "-10"
         loader.style.width = "0"
         loader.style.height = "0"
-    }, 5000)
+    }, 2000)
 };
 
 
 const listarSeries = async () => {
     try {
-        const response = await fetch('http://127.0.0.1:8000/api/listar/series/');
+        const response = await fetch('https://modulo-prescripcion.onrender.com/api/listar/series/');
+        // const response = await fetch('http://127.0.0.1:8000/api/listar/series/');
         const data = await response.json();
-
-
-
-
 
         let content = ``;
         data.series.forEach((serie, index) => {
             content += `
             <tr>
-            <td>${serie.titulo.toUpperCase()}</td>
-            <td>${serie.pais.toUpperCase()}</td>
+                <td>${serie.titulo.toUpperCase()}</td>
+                <td>${serie.pais.toUpperCase()}</td>
                 <td>
-                    <a href="detalleExplotacion/${serie.id}" class="btn btn-sm text-white" style="background-color: #ED7D31 !important">Detalle</a>
+                    <a href="/distribuciones/${serie.id}" class="btn btn-sm text-white" style="background-color: #ED7D31 !important">Detalle</a>
                 </td>
             </tr>
             `
         });
 
         if (tableBody_series) {
-
             if (data.series.length > 0) {
                 tableBody_series.innerHTML = content
             } else {
                 tableBody_series.innerHTML = 'NO HAY REGISTROS'
             }
         }
-
     } catch (ex) {
         alert(ex)
     }
@@ -96,12 +92,60 @@ window.addEventListener('load', async () => {
 
 
 
+dataTable = $("#series_old").DataTable({
+    language: {
+        "sProcessing": "Procesando...",
+        "sLengthMenu": "Mostrar _MENU_ registros",
+        "sZeroRecords": "No se encontraron resultados",
+        "sEmptyTable": "Ningún dato disponible en esta tabla",
+        "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+        "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+        "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+        "sInfoPostFix": "",
+        "sSearch": "Buscar:",
+        "sUrl": "",
+        "sInfoThousands": ",",
+        "sLoadingRecords": "Cargando...",
+        "oPaginate": {
+            "sFirst": "Primero",
+            "sLast": "Último",
+            "sNext": "Siguiente",
+            "sPrevious": "Anterior"
+        },
+        "oAria": {
+            "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+        }
+    }
+});
 
-// dataTable = $("#tablaSeries").DataTable({
-//     language: {
-//         url: "static/js/es-Es.json"
-//     }
-// });
+dataTable = $("#tablaDistribuciones").DataTable({
+    language: {
+        "sProcessing": "Procesando...",
+        "sLengthMenu": "Mostrar _MENU_ registros",
+        "sZeroRecords": "No se encontraron resultados",
+        "sEmptyTable": "Ningún dato disponible en esta tabla",
+        "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+        "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+        "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+        "sInfoPostFix": "",
+        "sSearch": "Buscar:",
+        "sUrl": "",
+        "sInfoThousands": ",",
+        "sLoadingRecords": "Cargando...",
+        "oPaginate": {
+            "sFirst": "Primero",
+            "sLast": "Último",
+            "sNext": "Siguiente",
+            "sPrevious": "Anterior"
+        },
+        "oAria": {
+            "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+        }
+    }
+});
+
 
 dataTable = $("#tablaExplotaciones").DataTable({
     language: {
